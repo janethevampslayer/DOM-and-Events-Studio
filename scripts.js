4b95886e9e4b7e58bb847dc419d026046c19a052
Jane Byrne <janebyrne92@gmail.com>
2019-11-25 19:12:31 -0600

    I just want to see if this works

--- scripts.js
+++ scripts.js
@@ -1,2 +1,34 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    console.log('window loaded');
+
+    document.getElementById("takeoff").addEventListener("click", function() {
+        let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        let flightStatus = document.getElementById("flightStatus");
+        let shuttleBackground = document.getElementById("shuttleBackground");
+        let shuttleHeight = document.getElementById("spaceShuttleHeight");
+        if (result = true) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            shuttleHeight.innerHTML = 10000;
+        }
+    });
+    document.getElementById("landing").addEventListener("click", function() {
+        let shuttleHeight = document.getElementById("spaceShuttleHeight");
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = "green";
+        shuttleHeight.innerHTML = 0;
+    });
+    document.getElementById("missionAbort").addEventListener("click", function() {
+        let shuttleHeight = document.getElementById("spaceShuttleHeight");
+        let result = window.alert("Confirm that you want to abort the mission.");
+        if (result = true) {
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackground.style.backgroundColor = "green";
+            shuttleHeight.innerHTML = 0;
+        }
+    });
+});
+
